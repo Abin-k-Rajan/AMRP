3bb49ff530104ce17f4a0bffcf36d4b5327da9c1
Abin <abin27577@gmail.com>
2022-02-02 00:48:06 +0530

    Gerneral count

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { apiUrl } from "../base";
 import './Styles/Footer.css'
 
 function Footer(){
+  const [generalCount, setGeneralCount] = useState([])
+
+  useEffect(() => {
+    fetch(`${apiUrl}crew/generalcount`).then(res => res.json()).then((result) => {
+      console.log(result)
+      setGeneralCount(result)
+    })
+  }, [])
+
     return (
         <>
             <div className='footer-container'>
@@ -13,68 +23,29 @@ function Footer(){
             <h2>Project Link</h2>
             <a href="https://github.com/Abin-k-Rajan/AMRP/tree/React-App" target="_blank">AMRP REACT APP</a>
             <a href="https://github.com/Abin-k-Rajan/AMRP/tree/Dotnet-Backend" target='_blank'>AMRP ASP.NET</a>
-            <a href="https://programmablesearchengine.google.com/cse/setup/basic?cx=6c1d57204d0f32abc" target='_blank'>Custom Serach Engine</a>
+            <a href="https://programmablesearchengine.google.com/" target='_blank'>Custom Serach Engine</a>
           </div>
           <div class='footer-link-items'>
             <h2>Contact Us</h2>
             <a href="https://www.linkedin.com/in/abin-k-732a18136/" target='_blank'>LinkedIn</a>
-            <a href="#" target='_blank'>LinkedIn</a>
+            <a href="https://www.linkedin.com/in/asprakash9/" target='_blank'>LinkedIn</a>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Page Details</h2>
+              <p>{generalCount[0].nomovies}   MOVIES | {generalCount[0].noactors} ACTORS</p>
+              <p>{generalCount[0].nowriters} WRITERS | {generalCount[0].nodirectors} DIRECTORS | {generalCount[0].nowriters} WRITERS</p>
           </div>
         </div>
         
       </div>
-      <section class='social-media'>
+      <section class='row d-flex align-items-center'>
         <div class='social-media-wrap'>
-          <div class='footer-logo'>
-            <Link to='/' className='social-logo'>
+          <div class='d-flex'>
+            <Link to='/' style={{"color": "white"}} className='social-logo'>
               AMRP
-              <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>All Movie Review Platform</small>
-          <div class='social-icons'>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </Link>
           </div>
-        </div>
       </section>
     </div>
         </>
